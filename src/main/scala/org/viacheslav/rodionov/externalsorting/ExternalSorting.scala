1f11ba97c9d03ca8a38048be17825128d0bf47f0
Viacheslav Rodionov <viacheslav.rodionov@gmail.com>
2016-10-31 01:23:38 +0100

    prefer File instead of path

--- src/main/scala/org/viacheslav/rodionov/externalsorting/ExternalSorting.scala
+++ src/main/scala/org/viacheslav/rodionov/externalsorting/ExternalSorting.scala
@@ -1,35 +1,58 @@
 package org.viacheslav.rodionov.externalsorting
 
-import java.io.PrintWriter
+import java.io.{File, PrintWriter}
 
 import org.viacheslav.rodionov.externalsorting.readers.FileStringReader
 
 import scala.collection.mutable
-import scala.collection.mutable.{Buffer, PriorityQueue}
+import scala.util.{Failure, Success, Try}
 
 case class ExternalSorting(inputFile: String, capacity: Int) extends ExternalAccess {
   lazy private val lines: Iterator[String] = fileLines(inputFile)
 
   def readIntoMemory: Iterator[String] = lines take capacity
 
+  def writeNextSortedBlock: Option[File] = {
+    readIntoMemory match {
+      case it if it.isEmpty => None
+      case nonEmptyIterator => {
+        import ExternalSorting._
+        val block: mutable.Buffer[String] = nonEmptyIterator.toBuffer
+        block.memorySort
+        ExternalSorting saveToFile block
+      }
+    }
+
+  }
 }
 
 object ExternalSorting extends ExternalAccess {
+  def apply(inputFile: File, capacity: Int): ExternalSorting =
+    ExternalSorting(inputFile.getAbsolutePath, capacity)
 
-  def saveToFile(sortedLines: mutable.Buffer[String]): String = {
-    val tempFile = java.io.File.createTempFile("extsrttbl", ".txt")
-    new PrintWriter(tempFile) {
-      sortedLines foreach { line => write(line + "\n") }
-      close
+  def saveToFile(sortedLines: mutable.Buffer[String]): Option[File] = {
+    Try {
+      val tempFile = java.io.File.createTempFile("extsrttbl", ".txt")
+      new PrintWriter(tempFile) {
+        sortedLines foreach { line => write(line + "\n") }
+        close
+      }
+      tempFile
+    } match {
+      case Success(outFile) => Some(outFile)
+      case Failure(exception) => {
+        exception.printStackTrace()
+        None
+      }
     }
-    tempFile.getAbsolutePath
   }
 
-  def merge(outFilePath: String, readers: FileStringReader*): Unit = {
+  def merge(outFile: File, files: File*): Unit = {
     implicit val ordering: Ordering[FileStringReader] = FileStringReader.ord
     val heap: mutable.PriorityQueue[FileStringReader] = mutable.PriorityQueue[FileStringReader]()
+    val readers = files map (f => FileStringReader(f))
     heap enqueue (readers: _*)
-    new PrintWriter(outFilePath) {
+    new PrintWriter(outFile) {
       while (heap.nonEmpty) {
         val minimal: FileStringReader = heap dequeue()
         minimal.currentHeadOption match {
